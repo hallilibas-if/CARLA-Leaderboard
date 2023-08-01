0bd953fb6236260ed23ec8cab06dcae1b0fe82f8
Shawan <shawan.mohammed@ice.rwth-aachen.de>
2023-08-01 11:48:53 +0200

    changed sh files

--- run_agent_2.sh
+++ run_agent_2.sh
@@ -4,7 +4,7 @@ export CARLA_ROOT=../CARLA_0.9.10.1           # change to where you installed CA
 export PORT=3500                                                    # change to port that CARLA is running on
 export ROUTES=./leaderboard/data/routes_training/route_21.xml         # change to desired route
 export TEAM_AGENT=shawan_agent.py                                    # no need to change
-export TEAM_CONFIG=./checkpointshawan/epoch=24.ckpt                                      # change path to checkpoint
+export TEAM_CONFIG=./checkpoints/epoch=24.ckpt                                      # change path to checkpoint
 export HAS_DISPLAY=1                                                # set to 0 if you don't want a debug window
 export REPS=10000
 export TRAFFIC=3700
